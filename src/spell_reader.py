7313318215da7ef28a9f40500357b371e2cc4d21
Koen Molenaar <k.molenaar@student.utwente.nl>
2020-04-13 20:57:42 +0200

    More test cases

--- src/spell_reader.py
+++ src/spell_reader.py
@@ -1,10 +1,10 @@
 import json
 import os
 
-def load_spells():
-    path = os.path.join(os.curdir, "..\\data\\spells.json")
+
+def load_spells(file_path):
+    path = os.path.join(os.curdir, file_path)
     file = open(path, "r")
     data = file.read()
     return len(data)
 
-print(f"{load_spells()}")
