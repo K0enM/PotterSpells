8e985a824d62075f4ac994532ac710ed5951d290
Koen Molenaar <k.molenaar@student.utwente.nl>
2020-04-13 21:40:35 +0200

    Add custom exception

--- src/cast_spell.py
+++ src/cast_spell.py
@@ -3,12 +3,16 @@ import pyttsx3
 import json
 
 
+class SpellNotFoundError(Exception):
+    pass
+
+
 def cast(spellname):
     spells = sr.load_spells("../data/spells.json")
     spell_data = json.loads(spells)
     for spell in spell_data["spells"]:
         if spell["incantation"] == spellname:
             pyttsx3.speak(spellname)
+            return 
+    raise SpellNotFoundError("That is not an existing spell!")
 
-
-cast("Accio")
